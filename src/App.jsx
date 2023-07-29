e8a9d8c1d7592fed6c0818888e670cefb7318e84
Sahil Savardekar <75142268+Sahil0511@users.noreply.github.com>
2023-07-29 23:26:03 +0530

    style : UserDetail UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import UserDetail from "./components/UserDetail";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <UserDetail />
     </>
   );
 };
---
+++ src/components/DeleteUser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteUser = () => {
+  return <></>;
+};
+
+export default DeleteUser;
